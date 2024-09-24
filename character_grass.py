35559d143a42ebd2c0c843ad3d531d26cc627277
pjs020304 <pjs020304@naver.com>
2024-09-24 10:51:01 +0900

    그리는 기초를 함수화

--- character_grass.py
+++ character_grass.py
@@ -5,57 +5,50 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def draw_base(cx, cy):
+    clear_canvas()
+    boy.draw_now(cx,cy)
+    grass.draw_now(400,40)
+    delay(0.01)
 
 def run_bottom1():
     cx=800//2
     cy=100
     while cx<800:
-        clear_canvas()
-        boy.draw_now(cx,cy)
+        draw_base(cx,cy)
         cx+=2
-        grass.draw_now(400, 40)
-        delay(0.01)
+
     pass
 def run_right():
     cx=800
     cy=100
     while cy<600:
-        clear_canvas()
-        boy.draw_now(cx,cy)
+        draw_base(cx,cy)
         cy+=2
-        grass.draw_now(400, 40)
-        delay(0.01)
+
     pass
 def run_top():
     cx=800
     cy=600
     while cx>0:
-        clear_canvas()
-        boy.draw_now(cx,cy)
+        draw_base(cx,cy)
         cx-=2
-        grass.draw_now(400, 40)
-        delay(0.01)
+
     pass
 def run_left():
     cx=0
     cy=600
     while cy>100:
-        clear_canvas()
-        boy.draw_now(cx,cy)
+        draw_base(cx,cy)
         cy-=2
-        grass.draw_now(400, 40)
-        delay(0.01)
+
     pass
 def run_bottom2():
     cx=0
     cy=100
     while cx<=400:
-        clear_canvas()
-        boy.draw_now(cx,cy)
+        draw_base(cx,cy)
         cx+=1
-        grass.draw_now(400, 40)
-
-        delay(0.01)
     pass
 
 def run_rectangle():
@@ -73,12 +66,9 @@ def run_circle():
     cx = 800/2
     cy = 600/2
     for theta in range(270,630):
-        clear_canvas()
         x = cx+r*math.cos(math.radians(theta))
         y = cy+r*math.sin(math.radians(theta))
-        boy.draw_now(x,y)
-        grass.draw_now(cx, 40)
-        delay(0.01)
+        draw_base(cx,cy)
     pass
 
 while True:
