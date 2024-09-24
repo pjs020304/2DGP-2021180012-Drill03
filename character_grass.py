d421b43ace4fdea95f274c15f7385bb7d834d663
pjs020304 <pjs020304@naver.com>
2024-09-24 10:19:59 +0900

    원 운동 그리기 제작완료

--- character_grass.py
+++ character_grass.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+import math
 open_canvas()
 
 grass = load_image('grass.png')
@@ -10,10 +10,17 @@ def run_rectangle():
     pass
 def run_circle():
     print('CIRCLE')
-    clear_canvas()
-    boy.draw_now(400,300)
-    grass.draw_now(400, 80)
-    delay(1)
+    
+    r= 200
+    cx = 800/2
+    cy = 600/2
+    for theta in range(270,630):
+        clear_canvas()
+        x = cx+r*math.cos(math.radians(theta))
+        y = cy+r*math.sin(math.radians(theta))
+        boy.draw_now(x,y)
+        grass.draw_now(cx, 40)
+        delay(0.01)
     pass
 
 while True:
