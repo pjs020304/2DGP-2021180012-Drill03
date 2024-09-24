5a35efe5b58227a2056b52212144e7ad0d5adfbd
pjs020304 <pjs020304@naver.com>
2024-09-24 10:02:00 +0900

    캐릭터 및 바닥 출력

--- character_grass.py
+++ character_grass.py
@@ -2,18 +2,23 @@ from pico2d import *
 
 open_canvas()
 
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
 def run_rectangle():
     print('RECTANGLE')
     pass
 def run_circle():
     print('CIRCLE')
+    clear_canvas()
+    boy.draw_now(400,300)
+    grass.draw_now(400, 80)
+    delay(1)
     pass
-
-grass = load_image('grass.png')
-boy = load_image('character.png')
 
 while True:
     run_circle()
     run_rectangle()
+    break
     
 close_canvas()
